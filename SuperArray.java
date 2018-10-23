f743650d3a95708de9825ec48df56ae1780b0c72
CodyBagwandeen <cbagwandeen@stuy.edu>
2018-10-23 11:50:49 -0400

    stuff from class

---
+++ SuperArray.java
@@ -0,0 +1,56 @@
+public class SuperArray {
+  private String[] data ;
+  private int size ;
+
+  public SuperArray(){
+    data = new String[10];
+  }
+
+  public void clear() {
+    size = 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  public boolean add( String element) {
+    if ( data.length > size) {
+      data[size] = element ;
+      size++;
+    }
+    return true;
+  }
+
+  public String toString() {
+    String output = "[";
+    for ( int x = 0 ; x < size ; x++) {
+      output += data[x];
+    }
+    return output + "]";
+  }
+
+  public String get( int index) {
+    if (index < 0 || index >= size()) {
+      return null;
+    } else {
+      return data[index];
+    }
+  }
+
+  public String set( int index, String element) {
+    String temp = "";
+    if  (index < 0 || index >= size()) {
+      return null;
+    } else {
+      temp = data[index];
+      data[index] = element;
+    }
+    return temp;
+  }
+
+}
---
+++ driver.java
@@ -0,0 +1,3 @@
+public class driver {
+  public void main
+}
